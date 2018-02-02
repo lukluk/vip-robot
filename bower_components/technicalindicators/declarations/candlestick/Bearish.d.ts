597ee151d15a18cad36319f8e4206f5662af587e
lukluk <lukluk.s@go-jek.com>
2018-02-02 11:48:20 +0700

    init

--- bower_components/technicalindicators/declarations/candlestick/Bearish.d.ts
+++
@@ -1,7 +0,0 @@
-import StockData from '../StockData';
-import CandlestickFinder from './CandlestickFinder';
-export default class BearishPatterns extends CandlestickFinder {
-    constructor();
-    hasPattern(data: StockData): boolean;
-}
-export declare function bearish(data: StockData): boolean;
--- bower_components/technicalindicators/src/Utils/NumberFormatter.ts
+++
@@ -1,8 +0,0 @@
-import { getConfig } from '../config';
-export function format(v:number):number {
-    let precision:number = getConfig('precision');
-    if(precision) {
-        return parseFloat(v.toPrecision(precision));
-    }
-    return v;
-}
